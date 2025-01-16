c74f20942d302ab465eb1f1eec771b3c619cc30a
Irany Luis-Gomez <iluisgomez8920@floridapoly.edu>
2025-01-16 16:30:36 +0000

    First Program

---
+++ 01_first_program/first_program.c
@@ -0,0 +1,18 @@
+//comment
+ 
+/*multiline comment*/
+
+#include <stdio.h>
+
+//Create a main function
+
+int main() {
+
+ printf("Welcome to COP2271\n");
+ printf("I want to code\n");
+
+ return 0;
+
+}
+
+
