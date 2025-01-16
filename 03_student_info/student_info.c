3d162afe51f4cda33ea85d917d10e2b8d4f2061a
Irany Luis-Gomez <iluisgomez8920@floridapoly.edu>
2025-01-16 16:31:55 +0000

    Student Info

---
+++ 03_student_info/student_info.c
@@ -0,0 +1,21 @@
+// stdio
+#include <stdio.h>
+
+//main function 
+int main(){
+
+    //Variables
+    char name[] = "Irany Luis-Gomez";
+    int course_id = 2271;
+    int student_id = 8920;
+    int difference = course_id - student_id;
+
+    //print
+    printf ("Name: %s\n", name);
+    printf ("Course ID: %d\n", course_id);
+    printf ("Student ID: %d\n", student_id);
+    printf ("Student ID - Course ID is: %d\n", difference);
+
+return 0;
+
+}
